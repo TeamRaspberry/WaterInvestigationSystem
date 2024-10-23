9f13c5895ce53c5e865f2b38ecfde5bc036f1e8a
Monoclocker <52466843+Monoclocker@users.noreply.github.com>
2024-10-23 11:40:38 +0300

    Delete AuthService directory

--- AuthService/AuthService.Core/DTOs/Roles/CreateRoleDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AuthService.Core.DTOs.Roles
-{
-    public class CreateRoleDTO
-    {
-        public required string name { get; set; }
-        public bool isDefault { get; set; }
-        public bool isAdmin { get; set; }
-    }
-}
--- AuthService/AuthService.Core/Interfaces/IDBMapper.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AuthService.Core.Interfaces
-{
-    public interface IDBMapper<CoreType, DBType>
-    {
-        DBType MapToDB(CoreType entity);
-        CoreType MapFromDB(DBType entity);
-    }
-}
